a6f749e52b72350c94638b55ac87eed4b79066fe
RoyalFlush <neji.kun.v@gmail.com>
2017-09-30 18:24:39 +0300

    update

---
+++ thread.c
@@ -0,0 +1,50 @@
+#
+#
+#
+#
+int j = 0;
+int counter = 0;
+long long int current_sum[10];
+
+struct params{
+	int left;
+	int right;
+};
+
+void* my_func(void *pointer)
+{	
+	int local_l;
+	int local_r;
+	params* x;
+	x = (params*)pointer;
+	local_l = x->left;
+	local_r = x->right;
+	while( j != (counter + pack
+}
+int main()
+{
+	int i = 0, R, L, pack, rest; 
+	int n = 0, fail = 0;
+
+	scanf("%d", &n);
+	scanf("%d%d", &L, &R);
+
+	j  == left;
+	pack = (R - L) / n;
+	rest = (R - L) % n;
+
+	for(i = 0; i < n; i++){
+		fail = pthread_create(&thread_id, 
+		(pthread_attr_t *)NULL,
+		my_func, (void*)&thread_id);
+	if(fail) {
+		printf("vse ploho");}
+	}
+
+
+
+
+
+	return 0;
+}
+
